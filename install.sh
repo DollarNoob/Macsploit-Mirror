e85463e7bc241ab59fe7d976493e4e5bebb696ac
DollarNoob <35988515+DollarNoob@users.noreply.github.com>
2025-10-16 18:07:46 +0900

    Rewrite installer

--- install.sh
+++ install.sh
@@ -1,122 +1,203 @@
 #!/bin/bash
 
-main() {
+base_url="https://raw.githubusercontent.com/DollarNoob/Macsploit-Mirror/main"
+
+print_title() {
     clear
-    echo -e "Welcome to the MacSploit Experience!"
-    echo -e "Install Script Version 2.6"
+    echo -e "\033[1;35m"
+    center "  __  __             _____       _       _ _    "
+    center " |  \/  |           / ____|     | |     (_) |   "
+    center " | \  / | __ _  ___| (___  _ __ | | ___  _| |_  "
+    center " | |\/| |/ _\` |/ __|\___ \| '_ \| |/ _ \| | __| "
+    center " | |  | | (_| | (__ ____) | |_) | | (_) | | |_  "
+    center " |_|  |_|\__,_|\___|_____/| .__/|_|\___/|_|\__| "
+    center "                          | |                   "
+    center "                          |_|                   "
+    echo -e "\033[0m"
+}
 
-    echo -ne "Checking License..."
-    curl -s "https://git.raptor.fun/main/jq-macos-amd64" -o "./jq"
-    chmod +x ./jq
-    
-    curl -s "https://git.raptor.fun/sellix/hwid" -o "./hwid"
-    chmod +x ./hwid
-    
-    local user_hwid=$(./hwid)
-    local hwid_info=$(curl -s "https://git.raptor.fun/api/whitelist?hwid=$user_hwid")
-    local hwid_resp=$(echo $hwid_info | ./jq -r ".success")
-    rm ./hwid
-    
-    if [ "$hwid_resp" != "true" ]
-    then
-        echo -ne "\rEnter License Key:       \b\b\b\b\b\b"
-        read input_key
-
-        echo -n "Contacting Secure Api... "
-        
-        local resp=$(curl -s "https://git.raptor.fun/api/sellix?key=$input_key&hwid=$user_hwid")
-        echo -e "Done.\n$resp"
-        
-        if [ "$resp" != 'Key Activation Complete!' ]
-        then
-            rm ./jq
-            exit
-            return
-        fi
-    else
-        local free_trial=$(echo $hwid_info | ./jq -r ".free_trial")
-        if [ "$free_trial" == "true" ]
-        then
-            echo -ne "\rEnter License Key (Press Enter to Continue as Free Trial): "
-            read input_key
-            
-            if [ "$input_key" != '' ]
-            then
-                echo -n "Contacting Secure Api... "
-                
-                local resp=$(curl -s "https://git.raptor.fun/api/sellix?key=$input_key&hwid=$user_hwid")
-                echo -e "Done.\n$resp"
-            fi
+check_requirements() {
+    local os_version=$(sw_vers -productVersion)
+    local architecture=$(uname -m)
+    if [ "$(echo "$os_version" | cut -d. -f1)" -ge 11 ]; then
+        if [ $architecture == "arm64" ]; then
+            curl -s "$base_url/jq-macos-arm64" -o "./jq"
+            center "✅ \033[1;36mmacOS $os_version\033[0m (Apple Silicon)"
         else
-            echo -e " Done.\nWhitelist Status Verified."
+            curl -s "$base_url/jq-macos-amd64" -o "./jq"
+            center "✅ \033[1;36mmacOS $os_version\033[0m (Intel)"
+        fi
+        chmod +x ./jq
+        echo ""
+        center "============================================================"
+        echo ""
+    else
+        if [ $architecture == "arm64" ]; then
+            center "❌ \033[1;31mmacOS $os_version\033[0m (Apple Silicon)"
+        else
+            center "❌ \033[1;31mmacOS $os_version\033[0m (Intel)"
+        fi
+        center "\033[31mYour device is not compatible with MacSploit.\033[0m"
+        center "\033[31mPlease upgrade to macOS 11.0+ if possible.\033[0m"
+        echo ""
+        exit
+    fi
+}
+
+check_permissions() {
+    if [[ ! -w ~/Downloads ]]; then
+        center "\033[31mTerminal is unable to access your Downloads folder.\033[0m"
+        center "\033[31mPlease grant Full Disk Access to Terminal and try again.\033[0m"
+        echo ""
+        open "x-apple.systempreferences:com.apple.preference.security?Privacy_AllFiles"
+        exit
+    fi
+
+    if [[ -d "/Applications/Roblox.app" ]]; then
+        if find "/Applications/Roblox.app" ! -exec test -w {} \; -print -quit | grep -q .; then
+            center "\033[31mTerminal is unable to access your current Roblox installation.\033[0m"
+            center "\033[31mPlease run the installer with sudo permissions.\033[0m"
+            echo ""
+            exit
         fi
     fi
 
-    echo -e "Downloading Latest Roblox..."
-    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
-    local robloxVersionInfo=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
-    local versionInfo=$(curl -s "https://git.raptor.fun/main/version.json")
-    
-    local mChannel=$(echo $versionInfo | ./jq -r ".channel")
-    local version=$(echo $versionInfo | ./jq -r ".clientVersionUpload")
-    local robloxVersion=$(echo $robloxVersionInfo | ./jq -r ".clientVersionUpload")
-    
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
-        curl "http://setup.rbxcdn.com/mac/$robloxVersion-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
-    else
-        curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    if [[ -d "./Applications/Roblox.app" ]]; then
+        if find "./Applications/Roblox.app" ! -exec test -w {} \; -print -quit | grep -q .; then
+            center "\033[31mTerminal is unable to access your current Roblox installation.\033[0m"
+            center "\033[31mPlease run the installer with sudo permissions.\033[0m"
+            echo ""
+            exit
+        fi
     fi
-    
-    echo -n "Installing Latest Roblox... "
-    [ -d "./Applications/Roblox.app" ] && rm -rf "./Applications/Roblox.app"
+
+    if [[ -d "/Applications/MacSploit.app" ]]; then
+        if find "/Applications/MacSploit.app" ! -exec test -w {} \; -print -quit | grep -q .; then
+            center "\033[31mTerminal is unable to access your current MacSploit installation.\033[0m"
+            center "\033[31mPlease run the installer with sudo permissions.\033[0m"
+            echo ""
+            exit
+        fi
+    fi
+
+    if [[ -d "./Applications/MacSploit.app" ]]; then
+        if find "./Applications/MacSploit.app" ! -exec test -w {} \; -print -quit | grep -q .; then
+            center "\033[31mTerminal is unable to access your current MacSploit installation.\033[0m"
+            center "\033[31mPlease run the installer with sudo permissions.\033[0m"
+            echo ""
+            exit
+        fi
+    fi
+}
+
+check_version() {
+    roblox_version_info=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
+    roblox_version=$(echo $roblox_version_info | ./jq -r ".clientVersionUpload")
+    center "✅ \033[1;34mRoblox\033[0m | $roblox_version"
+
+    version_info=$(curl -s "$base_url/version.json")
+    version=$(echo $version_info | ./jq -r ".clientVersionUpload")
+
+    if [ "$version" == "$roblox_version" ]; then
+        center "✅ \033[1;35mMacSploit\033[0m | $version"
+        sleep 1
+    else
+        center "❗ \033[1;35mMacSploit\033[0m | \033[33m$version\033[0m"
+        center "\033[33mMacSploit is not updated to the latest version of Roblox.\033[0m"
+        center "\033[33mThis does not mean MacSploit would not work at all.\033[0m"
+        center "\033[33mThanks to update hooks, MacSploit may still function for a few days.\033[0m"
+        echo ""
+        center "Do you want to proceed? (Y/N)"
+        read -n 1 -r answer
+        if [[ ! "$answer" =~ ^[Yy]$ ]]; then
+            echo -e "\n"
+            exit
+        fi
+    fi
+}
+
+install_roblox() {
+    print_title
+    center "📥 \033[1;34mDownloading RobloxPlayer...\033[0m"
+    echo ""
+    curl -# "https://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    echo ""
+
+    center "⚙️  \033[1;34mInstalling RobloxPlayer...\033[0m"
     [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
+    [ -d "./Applications/Roblox.app" ] && rm -rf "./Applications/Roblox.app"
 
     unzip -o -q "./RobloxPlayer.zip"
     mv ./RobloxPlayer.app /Applications/Roblox.app
     rm ./RobloxPlayer.zip
-    echo -e "Done."
-
-    echo -e "Downloading MacSploit..."
-    curl "https://git.raptor.fun/main/macsploit.zip" -o "./MacSploit.zip"
-
-    echo -n "Installing MacSploit... "
-    unzip -o -q "./MacSploit.zip"
-    echo -e "Done."
-
-    echo -n "Updating Dylib..."
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
-        curl -Os "https://git.raptor.fun/preview/macsploit.dylib"
-    else
-        curl -Os "https://git.raptor.fun/main/macsploit.dylib"
-    fi
-    
-    echo -e " Done."
-    echo -e "Patching Roblox..."
-    mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
-    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
-    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
-    rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
-    rm ./insert_dylib
-
-    echo -n "Installing MacSploit App... "
-    [ -d "./Applications/MacSploit.app" ] && rm -rf "./Applications/MacSploit.app"
-    [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
-    mv ./MacSploit.app /Applications/MacSploit.app
-    rm ./MacSploit.zip
-    
-    touch ~/Downloads/ms-version.json
-    echo $versionInfo > ~/Downloads/ms-version.json
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
-        cat <<< $(./jq '.channel = "previewb"' ~/Downloads/ms-version.json) > ~/Downloads/ms-version.json
-    fi
-    
-    rm ./jq
-    echo -e "Done."
-    echo -e "Install Complete! Developed by Nexus42!"
-    exit
 }
 
-main
+patch_roblox() {
+    print_title
+    center "📥 \033[1;35mDownloading MacSploit DYLIB...\033[0m"
+    echo ""
+    curl -#O "$base_url/macsploit.dylib"
+    curl -#O "$base_url/insert_dylib"
+    echo ""
+
+    center "⚙️  \033[1;35mPatching RobloxPlayer...\033[0m"
+    mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
+
+    local output=$(./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes)
+    if [[ "$output" != "Added LC_LOAD_DYLIB to /Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" ]]; then
+        center "\033[31mTerminal was unable to patch RobloxPlayer.\033[0m"
+        center "\033[31mThis is usually caused by anti-virus softwares.\033[0m"
+        center "\033[31mIf you have one running, please disable it and try again.\033[0m"
+        echo ""
+        exit
+    fi
+
+    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
+    rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
+}
+
+install_macsploit() {
+    print_title
+    center "📥 \033[1;35mDownloading MacSploit...\033[0m"
+    echo ""
+    curl -#O "$base_url/MacSploit.zip"
+    echo ""
+
+    center "⚙️  \033[1;35mInstalling MacSploit...\033[0m"
+    [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
+    [ -d "./Applications/MacSploit.app" ] && rm -rf "./Applications/MacSploit.app"
+
+    unzip -o -q "./MacSploit.zip"
+    mv ./MacSploit.app /Applications/MacSploit.app
+    rm ./MacSploit.zip
+}
+
+clean_up() {
+    print_title
+    center "🧹 \033[1;36mCleaning Up...\033[0m"
+    echo ""
+
+    touch ~/Downloads/ms-version.json
+    echo $version_info > ~/Downloads/ms-version.json
+
+    rm ./jq
+    rm ./insert_dylib
+    echo -e "Done."
+}
+
+center() {
+    local text="$1"
+    local cols=$(tput cols)
+    local plaintext=$(echo -e "$text" | sed "s/\x1B\[[0-9;]*m//g")
+    local pad=$(( (cols - ${#plaintext}) / 2 ))
+    printf "%*s%b\n" "$pad" "" "$text"
+}
+
+print_title
+check_requirements
+check_permissions
+check_version
+install_roblox
+patch_roblox
+install_macsploit
+clean_up
