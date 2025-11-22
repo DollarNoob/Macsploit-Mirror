ad5ef2004f0f6bf9eace531e4d5a869a709866ee
DollarNoob <35988515+DollarNoob@users.noreply.github.com>
2025-11-23 07:49:42 +0900

    Try to fix perm issue

--- install.sh
+++ install.sh
@@ -289,8 +289,8 @@ clean_up() {
         center "\033[31mTerminal is unable to access your Downloads folder.\033[0m"
         center "\033[31mPlease input your password to run the installer with sudo permissions.\033[0m"
         echo
-        sudo touch ./Downloads/ms-version.json
-        sudo echo $version_info > ./Downloads/ms-version.json
+        sudo touch "$HOME/Downloads/version.json"
+        sudo echo $version_info > "$HOME/Downloads/version.json"
     else
         echo $version_info > ./Downloads/ms-version.json
     fi
