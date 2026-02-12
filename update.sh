205aabdffea7e5179796cca481fbc417c9f5ff2d
DollarNoob <35988515+DollarNoob@users.noreply.github.com>
2026-02-12 09:09:02 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ curl -# https://git.raptor.fun/main/version.json -o ./version.json
 curl -# https://git.raptor.fun/main/macsploit.dylib -o ./macsploit_x86_64.dylib
 curl -# https://git.raptor.fun/arm/macsploit.dylib -o ./macsploit_arm64.dylib
 
-rm -rf MacSploit.zip
+rm -f ./MacSploit.zip
 mkdir macsploit
 cd macsploit
 curl -# https://git.raptor.fun/main/macsploit.zip -o ./macsploit.zip
